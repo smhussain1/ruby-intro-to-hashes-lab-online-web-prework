9504e5bc53ab003a661797f9967f151a475814ba
michael hussain <smhussain_1@yahoo.com>
2019-01-03 18:07:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,7 @@ def monopoly_with_third_tier
 	monopoly = { :railroads => 
 	            {:pieces => 4, 
 	            :names => {
+	              :reading_railroad => {},
 	              
 	            }, 
 	            :rent_in_dollars => {
